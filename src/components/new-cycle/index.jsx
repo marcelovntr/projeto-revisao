f6376da346f365760e9b454a2b66a67a6e8e65ff
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-08-26 20:29:51 -0300

    quase nada de mudanças

--- src/components/new-cycle/index.jsx
+++ src/components/new-cycle/index.jsx
@@ -9,12 +9,23 @@ export function NewCycle() {
 
     //console.log(ref.current?.value)
 
-    const { register } = useFormContext()
+    const { register, formState } = useFormContext()
 
     return (
         <div className='container--new-cycle'>
             <label htmlFor="task">Vou trabalhar em</label>
-            <input type="text" id="task" placeholder='Criar timer365...' {...register('task')} />
+            <input type="text" id="task" placeholder='Criar timer365...'
+                {...register('task',
+                    {
+                        required: { value: true, message: 'campo obrigatório!' },
+                        minLength: { value: 3, message: 'mínimo de 3 caracteres para a tarefa!' }
+
+                    })}
+            />
+            {formState.errors.task && (<p className='text--error'>
+                {formState.errors.task.message}
+            </p>)}
+           
             {/*<input ref={ref} type="text" id="task" placeholder='Criar timer365...' />*/}
 
             <label htmlFor="minutesAmount">durante</label>
